6a72055bd8764b1ff352fa34ae73e8210327b9e9
desecaw13 <47424966+desecaw13@users.noreply.github.com>
2021-10-01 19:44:55 -0700

    a comma

--- elgg/mod/pen-theme/elgg-plugin.php
+++ elgg/mod/pen-theme/elgg-plugin.php
@@ -19,7 +19,7 @@ return [
 		// global font family  todo: add my font?
 		'font-family' => 'BlinkMacSystemFont, -apple-system, "Segoe UI", "Roboto", "Oxygen", "Ubuntu", "Cantarell", "Fira Sans", "Droid Sans", "Helvetica Neue", "Helvetica", "Arial", sans-serif',
 
-		'anchor-color' => '#784591'
+		'anchor-color' => '#784591',
 		'anchor-color-hover' => '#3c2248',
 
 		// element colors
